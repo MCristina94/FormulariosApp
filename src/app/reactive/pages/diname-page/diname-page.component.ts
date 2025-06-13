44658dcaf94ac15f4849acaaeb50d946ebddab23
Maureen Garcia <cmaureen1908@gmail.com>
2025-06-13 18:42:59 -0500

    finished dynamic form component

--- src/app/reactive/pages/diname-page/diname-page.component.ts
+++ src/app/reactive/pages/diname-page/diname-page.component.ts
@@ -1,9 +1,49 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormUtils } from '../../../utils/form-utils';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-diname-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './diname-page.component.html',
 })
-export class DinamePageComponent {}
+export class DinamePageComponent {
+  private fb = inject(FormBuilder);
+  FormUtils = FormUtils;
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array(
+      [
+        ['Metal Gear', Validators.required],
+        ['Guitar Hero', Validators.required],
+      ],
+      Validators.minLength(3)
+    ),
+  });
+
+  newFavoriteGame = new FormControl('', Validators.required); //para el campo agregar
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  onAddToFavorites() {
+    if (this.newFavoriteGame.invalid) return;
+    const newGame = this.newFavoriteGame.value;
+
+    this.favoriteGames.push(this.fb.control(newGame, Validators.required));
+    this.newFavoriteGame.reset();
+  }
+
+  onDeleteFavorite(index: number) {
+    this.favoriteGames.removeAt(index);
+  }
+}
--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,4 +1,4 @@
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 export class FormUtils {
   static isValidField(form: FormGroup, fieldName: string): boolean | null {
@@ -10,6 +10,28 @@ export class FormUtils {
   static getFieldError(form: FormGroup, fieldName: string): string | null {
     const control = form.controls[fieldName] as FormControl;
     if (!control || !control.errors) return null;
+
+    const errors = control.errors ?? {};
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return ` Minimo de ${errors['minlength'].requiredLength} caracteres`;
+        case 'min':
+          return `Valor minimo de ${errors['min'].min}`;
+      }
+    }
+    return null;
+  }
+
+  static isValidFieldInArray(form: FormArray, index: number) {
+    return form.controls[index].errors && form.controls[index].touched;
+  }
+
+  static getFieldErrorInArray(form: FormArray, index: number): string | null {
+    const control = form.controls[index] as FormControl;
+    if (form.controls.length === 0) return null;
 
     const errors = control.errors ?? {};
     for (const key of Object.keys(errors)) {
