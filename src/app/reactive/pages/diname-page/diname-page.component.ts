2a6f2bab57fab9f15fdaac0162ab0637855a9721
Maureen Garcia <cmaureen1908@gmail.com>
2025-06-13 10:18:55 -0500

    created the infraestructure

---
+++ src/app/reactive/pages/diname-page/diname-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diname-page',
+  imports: [],
+  templateUrl: './diname-page.component.html',
+})
+export class DinamePageComponent { }
