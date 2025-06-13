52a84e821fa200c6be01e2cf9907e300f4d18e52
Maureen Garcia <cmaureen1908@gmail.com>
2025-06-13 14:39:10 -0500

    added navbar or sidemenu

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
+import { reactiveRoutes } from '../../../reactive/reactive.routes';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+const reactiveItems = reactiveRoutes[0].children ?? []; //la ruta en la posicion 0 es la ruta padre
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+  reactiveMenu: MenuItem[] = reactiveItems
+    .filter((item) => item.path !== '**')
+    .map((item) => ({
+      route: `reactive/${item.path}`,
+      title: `${item.title}`,
+    }));
+
+  authMenu: MenuItem[] = [
+    {
+      title: 'Registro',
+      route: './auth',
+    },
+  ];
+
+  country: MenuItem[] = [
+    {
+      title: 'Paises',
+      route: './country',
+    },
+  ];
+}
