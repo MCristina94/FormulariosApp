6063437299a6f2690eed64e45240550147f900b2
Maureen Garcia <cmaureen1908@gmail.com>
2025-06-13 19:43:59 -0500

    added validators to passwords

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,6 +1,17 @@
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
 
 export class FormUtils {
+  //expresiones regulares:
+
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
+
   static isValidField(form: FormGroup, fieldName: string): boolean | null {
     return (
       !!form.controls[fieldName].errors && form.controls[fieldName].touched
@@ -20,6 +31,12 @@ export class FormUtils {
           return ` Minimo de ${errors['minlength'].requiredLength} caracteres`;
         case 'min':
           return `Valor minimo de ${errors['min'].min}`;
+        case 'email':
+          return `El correo es invalido`;
+        case 'pattern':
+          if (errors['pattern'].requiredPattern === FormUtils.emailPattern) {
+            return 'El correo electronico no es permitido';
+          }
       }
     }
     return null;
@@ -45,5 +62,15 @@ export class FormUtils {
       }
     }
     return null;
+  }
+
+  static isFieldOneEqualFieldTwo(field1: string, field2: string) {
+    //funcion para validar si password1 es igual a password2
+    return (formGroup: AbstractControl) => {
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      return field1Value === field2Value ? null : { passwordsNotEqual: true };
+    };
   }
 }
